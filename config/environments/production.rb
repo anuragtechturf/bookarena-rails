97a0027a8b57458787dccce98000a89fd006c28b
Jeet <jeetharia.turf@gmail.com>
2025-08-03 08:24:46 +0000

    prod masterkey

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  #fortest
+  config.require_master_key = false
+
+
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
